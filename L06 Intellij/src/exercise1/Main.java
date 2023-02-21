8882895fcb85bbe849415bd7d6de96c73f24f8bc
Martin <77696248+martinersej@users.noreply.github.com>
2023-02-21 13:31:10 +0100

    Update Main.java

--- L06 Intellij/src/exercise1/Main.java
+++ L06 Intellij/src/exercise1/Main.java
@@ -22,5 +22,38 @@ public class Main {
         System.out.println(Arrays.toString(cList));
 
         //d
+        int[] dList = new int[10];
+        for (int i = 1; i <= 10; i++) {
+            dList[i-1] = i*2;
+        }
+        System.out.println(Arrays.toString(dList));
+
+        //e
+        int[] eList = new int[10];
+        for (int i = 1; i <= 10; i++) {
+            eList[i-1] = i*i;
+        }
+        System.out.println(Arrays.toString(eList));
+
+        //f
+        int[] fList = new int[10];
+        for (int i = 1; i < 10; i+= 2) {
+            fList[i] = 1;
+        }
+        System.out.println(Arrays.toString(fList));
+
+        //g
+        int[] gList = new int[10];
+        for (int i = 0; i < 10; i++) {
+            gList[i] = (i < 5) ? i : i-5;
+        }
+        System.out.println(Arrays.toString(gList));
+
+        //h
+        int[] hList = new int[10];
+        for (int i = 1; i <= 10; i++) {
+            hList[i] = (i%2 == 0) ? hList[i-1]+3 : hList[i-1]+1;
+        }
+        System.out.println(Arrays.toString(hList));
     }
 }
